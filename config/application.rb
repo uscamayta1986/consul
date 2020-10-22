342e6f4f5eaa682b28f546fe3f21ed0eaae303cc
jaime uscamayta <uscamayta1986@gmail.com>
2020-10-22 19:31:53 +0000

    xcv

--- config/application.rb
+++ config/application.rb
@@ -44,8 +44,8 @@ module Consul
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
     available_locales = [
-      "ar",
-      "bg",
+      #"ar",
+      #"bg",
       "bs",
       "ca",
       "cs",
